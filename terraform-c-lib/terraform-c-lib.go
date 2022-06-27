ea1da9948ecf9928c5d0b4ee88a38b01f789a6f9
John V. Busch <john@johnvbusch.com>
2022-06-27 14:08:49 -0700

    basic library building, not yet tested (at all)

---
+++ terraform-c-lib/terraform-c-lib.go
@@ -0,0 +1,33 @@
+package main
+
+import (
+	"C"
+	"log"
+	"fmt"
+	//	"github.com/hashicorp/terraform/internal/configs"
+	"github.com/hashicorp/terraform/internal/configs/configload"
+
+)
+
+//export LoadModule
+func LoadModule(modulePath string) (rc int, config_c *C.char, errstr *C.char) {
+	log.Printf("modulePath: %s", modulePath)
+	modulePathClean := modulePath[:len(modulePath)-1]
+
+	config := &configload.Config{}
+	loader, loaderErr := configload.NewLoader(config)
+	if loaderErr != nil {
+		log.Printf("Error creating loader: %s", loaderErr )
+	}
+
+
+	loadedConfig, configErr := loader.LoadConfig(modulePathClean)
+	if configErr != nil {
+		log.Printf("Error loading config: %s", configErr )
+	}
+	log.Printf("Module is %#v", loadedConfig.Module)
+	return 0, C.CString(fmt.Sprintf("%v", loadedConfig.Module)), nil
+}
+
+func main() {}
+
